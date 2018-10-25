a582bb305444a87d4ab7d207d4d5bf668f73203e
houshanyun <likeredbean@gmail.com>
2018-10-25 23:58:10 +0800

    a1

---
+++ kwin1.py
@@ -0,0 +1,37 @@
+#!usr/bin/python3
+
+import random
+import pandas as pd
+import os
+
+def key_count(x1):   
+    def key_in():
+        n = int(input('count: '))
+        for i in range(n):
+            x1()
+    return key_in
+
+        
+@key_count
+def word_read():    
+    w = random.choice(words)
+    print(w)
+    keyin = input('word: ')
+    if keyin == 'end':
+        pass
+    elif keyin == w[0]:
+        print('ok')
+    else:
+        print('error')
+    
+filepath = os.path.dirname(os.path.abspath(__file__))
+allpath = os.path.join(filepath, 'words.csv')
+df1 = pd.read_csv(allpath)
+df1.columns = ['1', '2', 'en', 'tw' ]
+a = df1.loc[:, 'en']
+b = df1.loc[:,'tw']
+c = zip(a, b)
+words = list(c)
+    
+word_read()
+
