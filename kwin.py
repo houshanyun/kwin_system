4b659743bdd1c52d5f1f0ac0f647a551acae1ee8
houshanyun <likeredbean@gmail.com>
2018-10-27 19:27:29 +0800

    b1

--- kwin.py
+++ kwin.py
@@ -7,25 +7,42 @@ import sys
 
 def key_count(x1):   
     def key_in():
-        n = int(input('count: '))
-        for i in range(n):
-            x1()
+        try:
+            n = input('count: ')
+            if n == 'end':
+                sys.exit()
+        except ValueError:
+            print('請輸入整數！')
+            n = input('count: ')
+        error = 0
+        for i in range(int(n)):
+            error += x1()
+        if error == 0:
+            print('恭喜你！太完美了！')
+        else:
+            print(f'可惜錯了{error}個！')
     return key_in
 
-        
+
 @key_count
-def word_read():    
+def word_read():
+    error_count = 0        
     w = random.choice(words)
-    print(w)
-    keyin = input('word: ')
-    if keyin == 'end':
+    print(f'EN: {w[0]}\nTW: {w[1]}')
+    keyin = input('keyin: ')
+    if keyin.lower() == 'end':
         print('close……')
         sys.exit()
-    elif keyin == w[0]:
+    elif keyin.lower() == w[0].lower():
         print('ok')
     else:
         print('error')
-    
+        error_count = 1
+    return error_count
+        
+
+print("請輸入練習次數 or 輸入'end'離開程式")
+
 filepath = os.path.dirname(os.path.abspath(__file__))
 allpath = os.path.join(filepath, 'words.csv')
 df1 = pd.read_csv(allpath)
