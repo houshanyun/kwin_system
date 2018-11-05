d3379d9a183a4cffb3f3cff49269372c94543fda
houshanyun <likeredbean@gmail.com>
2018-11-06 07:21:32 +0800

    c2

--- test/sqlfunc.py
+++ test/sqlfunc.py
@@ -1,12 +1,16 @@
 import sqlite3 as sql
 import os
+import pandas as pd
 
 filepath = os.path.dirname(os.path.abspath(__file__))
 dbpath = os.path.join(filepath, 'words.db')
-print(dbpath)
 
 
 conn = sql.connect(dbpath)
 cutsor = conn.cursor()
-print(cutsor)
+a = conn.execute('select * from wordsone')
+data = a.fetchall()
+print(data)
+ds = pd.DataFrame(data)
+
 conn.close()
